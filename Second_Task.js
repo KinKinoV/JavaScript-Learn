9ebc996f474c9c1080379912103c045a8ef278da
KinKinoV <mr.kolbasa.ask@gmail.com>
2022-09-17 20:44:28 +0300

    Completed second task

---
+++ Second_Task.js
@@ -0,0 +1,15 @@
+/* Task 2:
+    Declare three variables:
+        1) variable for the name;
+        2) variable for the surname;
+        3) variable for the age;
+    Using console.log() commands and by using the variables
+    output to the log such phrase:
+        My name is *NAME* *SURNAME*, I'm *YEARS* old.
+*/
+
+var myName = 'Zhenia';
+var surname = 'Kolbasenko';
+var age = '21';
+
+console.log(`My name is ${myName} ${surname}, I'm ${age} years old.`)
